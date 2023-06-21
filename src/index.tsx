0f3c3c0e829da6ddbf5501273915de334837884d
Gabriel Peixoto <peixoto.gabriel@hotmail.com>
2023-06-21 10:43:32 -0300

    Fix(typing)

--- src/index.tsx
+++ src/index.tsx
@@ -1,50 +1,48 @@
-import { NativeModules, NativeEventEmitter, Platform } from 'react-native';
+import { NativeModules, NativeEventEmitter, Platform } from 'react-native'
 
 const LINKING_ERROR =
-  `The package 'react-native-rfid8500-zebra' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  'The package \'react-native-rfid8500-zebra\' doesn\'t seem to be linked. Make sure: \n\n' +
+  Platform.select({ ios: '- You have run \'pod install\'\n', default: '' }) +
   '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+  '- You are not using Expo Go\n'
 
 const Rfid8500Zebra = NativeModules.Rfid8500Zebra
   ? NativeModules.Rfid8500Zebra
   : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+    {},
+    {
+      get() {
+        throw new Error(LINKING_ERROR)
+      },
+    }
+  )
 
-const eventEmitter = new NativeEventEmitter();
+const eventEmitter = new NativeEventEmitter()
 
-console.log(eventEmitter)
+const events: any = {}
 
-const events: any = {};
-
-Rfid8500Zebra.on = (event: any, handler: any) => {
-  const eventListener = eventEmitter.addListener(event, handler);
+Rfid8500Zebra.on = (event: string, handler: (data: any) => void) => {
+  const eventListener = eventEmitter.addListener(event, handler)
 
   events[event] = events[event]
     ? [...events[event], eventListener]
-    : [eventListener];
-};
+    : [eventListener]
+}
 
-Rfid8500Zebra.off = (event: any) => {
+Rfid8500Zebra.off = (event: string) => {
   if (Object.hasOwnProperty.call(events, event)) {
-    const eventListener = events[event].shift();
+    const eventListener = events[event].shift()
 
-    if (eventListener) eventListener.remove();
+    if (eventListener) eventListener.remove()
   }
-};
+}
 
-Rfid8500Zebra.removeAll = (event: any) => {
+Rfid8500Zebra.removeAll = (event: string) => {
   if (Object.hasOwnProperty.call(events, event)) {
-    eventEmitter.removeAllListeners(event);
+    eventEmitter.removeAllListeners(event)
 
-    events[event] = [];
+    events[event] = []
   }
-};
+}
 
-export default Rfid8500Zebra;
+export default Rfid8500Zebra
