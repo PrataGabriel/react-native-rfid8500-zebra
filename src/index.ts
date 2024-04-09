034710949ad43d11d6d3f440a4a54381dd8cbee6
Gabriel Peixoto <peixoto.gabriel@hotmail.com>
2024-04-09 17:16:09 -0300

    feat: update and fix typings

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,59 @@ import {
   PermissionsAndroid,
 } from 'react-native';
 
-import type {
-  DeviceDetailsType,
-  DevicesType,
-  EventsType,
-  HandlerType,
-  IwriteTagStatusState,
-  RfidZebraType,
-} from './internal/types';
+import type { EmitterSubscription } from 'react-native';
+
+export interface HandlerType {
+  error?: string;
+  status?: boolean;
+  distance?: number;
+  level?: number;
+  cause?: string;
+}
+
+export interface DevicesType {
+  name: string;
+  mac: string;
+}
+
+export interface DeviceDetailsType {
+  antennaLevel?: number;
+  name?: string;
+  mac?: string;
+  batteryLevel?: number;
+}
+
+export interface EventsType {
+  [key: string]: EmitterSubscription[];
+}
+
+export interface RfidZebraType {
+  on: (
+    event: string,
+    handler: (data: string | string[] | HandlerType) => void
+  ) => void;
+  off: (event: string) => void;
+  removeAll: (event: string) => void;
+  isConnected: () => Promise<boolean>;
+  getDevices: () => Promise<DevicesType[]>;
+  disconnect: () => Promise<boolean>;
+  reconnect: () => void;
+  connect: (name: string, mac: string) => Promise<boolean>;
+  getDeviceDetails: () => Promise<DeviceDetailsType>;
+  clear: () => void; // limpa as tags lida da memória
+  setSingleRead: (enable: boolean) => void;
+  setAntennaLevel: (antennaLevel: number) => Promise<boolean>;
+  programTag: (oldTag: string, newTag: string) => Promise<boolean>; // Mudar o valor de uma tag
+  setEnabled: (enable: boolean) => Promise<boolean>; // Ativa o modo de leitura RFID = true // ativa o modo leitura BARCODE = false
+  enableLocateTag: (enable: boolean, tag: string) => Promise<boolean>;
+  softReadCancel: (enable: boolean) => Promise<void>; // Ativa ou desativa o modo leitura
+  setSession: (session: number) => Promise<boolean>;
+}
+
+export interface IwriteTagStatusState {
+  status?: boolean;
+  error?: string;
+}
 
 const LINKING_ERROR =
   "The package 'react-native-rfid8500-zebra' doesn't seem to be linked. Make sure: \n\n" +
